b55b092526a1c4ecb6e57890a92da9ef15b71a67
Jacob <leonardofaria_7@hotmail.com>
2019-08-02 20:30:54 -0300

    Exercicio 1 - TypeScript 02/08/19

---
+++ dist/exercicio1.js
@@ -0,0 +1,11 @@
+var nota1 = 10;
+var nota2 = 8;
+var nota3 = 6;
+var media = (nota1 + nota2 + nota3) / 3;
+console.log("A média é " + media);
+if (media >= 7) {
+    console.log("Você foi aprovado :)");
+}
+else {
+    console.log("Você foi reprovado :(");
+}
---
+++ exercicio1.ts
@@ -0,0 +1,15 @@
+let nota1 : number = 10
+let nota2 : number = 8
+let nota3 : number = 6
+
+let media : number = (nota1 + nota2 + nota3) /3
+
+console.log("A média é " + media)
+
+if(media >= 7) {
+    console.log("Você foi aprovado :)")
+}
+else {
+    console.log("Você foi reprovado :(")
+}
+
