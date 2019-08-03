28c7bce514202153149077e86d6be66761da2703
Jacob <leonardofaria_7@hotmail.com>
2019-08-02 21:41:00 -0300

    Exercicios Folhas de papel - TypeScript

---
+++ dist/exercicio2.js
@@ -0,0 +1,10 @@
+var competidores = 33;
+var qtdfolhas = 10;
+var folhaspcomp = 3;
+var resultado = qtdfolhas / folhaspcomp;
+if (resultado > competidores) {
+    console.log("SIM");
+}
+else {
+    console.log("NÃO");
+}
---
+++ exercicio2.ts
@@ -0,0 +1,12 @@
+let competidores : number = 33
+let qtdfolhas : number = 10
+let folhaspcomp : number = 3
+
+let resultado : number = qtdfolhas / folhaspcomp
+
+if (resultado > competidores){
+    console.log("SIM")
+}
+else{
+    console.log("NÃO")
+}
